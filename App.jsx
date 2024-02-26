a5d4d6baadcfe5f5130d00de1205bdc70baf33d1
Ganga-tharank <154905189+Ganga-tharank@users.noreply.github.com>
2024-02-26 07:52:51 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import './App.css'
+// import  Cardcomponents from "./cardcomponents";
+
+
+const App = () => {
+  return (
+
+    <div className='row'>
+           <div className='container'>
+          <div className='col-4'>
+          <div className='card'>
+     
+       <div className='card-header'>
+         <h5>FREE</h5>
+         <h1>$0/month</h1><hr></hr>
+       </div>
+       <ul className='menu-list'>
+         <li>single user</li>
+         <li>50GB storage</li>
+         <li>Unlimited public project</li>
+         <li>Community Acess</li>
+          <li>Unlimited private Projects</li>
+         <li>Dedicated phone support</li>
+         <li>Free subdomain</li>
+         <li>Monthly status Project</li>
+       </ul>
+ 
+       <button className='btn'>Button</button>
+       
+     </div>
+      </div>
+          <div className='col-4'>
+          <div className='card'>
+     
+       <div className='card-header'>
+         <h5>PLUS</h5>
+         <h1>$9/month</h1><hr></hr>
+       </div>
+       <ul className='menu-list'>
+         <li>5 user</li>
+         <li>50GB storage</li>
+         <li>Unlimited public project</li>
+         <li>Community Acess</li>
+          <li>Unlimited private Projects</li>
+         <li>Dedicated phone support</li>
+         <li>Free subdomain</li>
+         <li>Monthly status Project</li>
+       </ul>
+ 
+       <button className='btn'>Button</button>
+       
+     </div>
+      </div>
+          <div className='col-4'>
+          <div className='card'>
+     
+       <div className='card-header'>
+         <h5>PRO</h5>
+         <h1>$49/month</h1><hr></hr>
+       </div>
+       <ul className='menu-list'>
+         <li>Unlimited user</li>
+         <li>50GB storage</li>
+         <li>Unlimited public project</li>
+         <li>Community Acess</li>
+          <li>Unlimited private Projects</li>
+         <li>Dedicated phone support</li>
+         <li>Free subdomain</li>
+         <li>Monthly status Project</li>
+       </ul>
+ 
+       <button className='btn'>Button</button>
+       
+     </div>
+      </div>
+            
+          </div>
+          
+        </div>
+    
+
+    
+
+
+       
+    
+    
+
+    
+
+    
+  );
+};
+
+
+
+export default App;
